1884ce6689463c3edae30c6925426e1c1d078df5
frekerzzzz <kannamugesh135@gmail.com>
2023-12-26 18:13:57 +0530

    updated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,20 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="Kannamugesh135@gmail.com@email.com"></a>
+          <a href="mailto:kannamugesh135@gmail.com" target="_blank">Give a Mail</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/mugesh-kanna-506754247/"></a>
+          <a href="https://www.linkedin.com/in/mugesh-kanna-506754247/" target="_blank">
+            Linked In
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/Mugesz"></a>
+          <a href="https://github.com/Mugesz" target="_blank">Github</a>
         </li>
       </ul>
     </footer>
